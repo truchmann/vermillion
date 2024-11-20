af4e722f82ff7283be39f571af988a635f4663d8
truchmann <truchmann28@gmail.com>
2024-11-20 17:38:15 +0500

    Update and rename README.md to pipipupupu.js

---
+++ pipipupupu.js
@@ -0,0 +1,9 @@
+﻿const cities = []
+const names = ["Rome", "Amsterdam", "Lisbon", "Frankfurt", "Prague"]
+
+for (let i = 0; i < names.length; i++) {  
+    console.log(names[i]); 
+    cities[i] = names[i];   
+}
+
+# vermillion
